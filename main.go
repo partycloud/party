80291acc874cf0798fecfbaec87aada5bd33aabe
Dave Newman <ddangerous@gmail.com>
2017-05-17 19:34:30 -0700

    Refactoring

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os/exec"
-	"strings"
 
 	"github.com/getlantern/systray"
 )
@@ -16,54 +12,6 @@ var knownNetworks = map[string]string{
 	"8056c2e21c000001": "Mostly Aussies",
 }
 var connectedNetworks []GroupNetwork
-
-type GroupNetwork struct {
-	ID string
-}
-
-func ListGroupNetworks() ([]GroupNetwork, error) {
-	cmd := exec.Command("zerotier-cli", "listnetworks")
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		return nil, err
-	}
-
-	if err := cmd.Start(); err != nil {
-		return nil, err
-	}
-
-	lines := bufio.NewScanner(stdout)
-	lCount := -1
-	groups := make([]GroupNetwork, 0)
-	for lines.Scan() {
-		lCount++
-		// skip first line
-		if lCount > 0 {
-			// 3rd word is our guy
-			id := strings.Split(lines.Text(), " ")[2]
-			groups = append(groups, GroupNetwork{ID: id})
-		}
-	}
-	if err := lines.Err(); err != nil {
-		return nil, err
-	}
-
-	if err := cmd.Wait(); err != nil {
-		log.Fatal(err)
-	}
-
-	return groups, nil
-}
-
-func JoinGroup(id string) error {
-	fmt.Printf("Joining %s\n", id)
-	return exec.Command("zerotier-cli", "join", id).Run()
-}
-
-func LeaveGroup(id string) error {
-	fmt.Printf("Leaving %s\n", id)
-	return exec.Command("zerotier-cli", "leave", id).Run()
-}
 
 func onReady() {
 	systray.SetTitle("⛅️")
---
+++ zerotier.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os/exec"
+)
+
+type GroupNetwork struct {
+	ID string
+}
+
+func ListGroupNetworks() ([]GroupNetwork, error) {
+	out, err := exec.Command("zerotier-cli", "-j", "listnetworks").Output()
+	if err != nil {
+		return nil, err
+	}
+
+	var nets []GroupNetwork
+	err = json.Unmarshal(out, &nets)
+	return nets, err
+}
+
+func JoinGroup(id string) error {
+	fmt.Printf("Joining %s\n", id)
+	return exec.Command("zerotier-cli", "join", id).Run()
+}
+
+func LeaveGroup(id string) error {
+	fmt.Printf("Leaving %s\n", id)
+	return exec.Command("zerotier-cli", "leave", id).Run()
+}
